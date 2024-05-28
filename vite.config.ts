4838305457253c09d81d64343f5c4693784f76d7
Mateusz Brzeziński <mateusz0brzezinski@gmail.com>
2024-05-28 22:48:28 +0200

    build: update vite and ts config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,22 @@
+import MillionLint from '@million/lint';
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import * as path from 'path';
 
 // https://vitejs.dev/config/
+const plugins = [react()];
+plugins.unshift(MillionLint.vite())
 export default defineConfig({
-  plugins: [react()],
+  plugins: plugins,
   resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+    alias: [{
+      find: "@",
+      replacement: path.resolve(__dirname, "src")
+    }]
   },
   build: {
     rollupOptions: {
       input: ['src/main.tsx', './index.html']
-    },
-  },
+    }
+  }
 });
