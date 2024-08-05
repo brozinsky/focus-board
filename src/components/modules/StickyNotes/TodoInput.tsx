24d5c16905e9ed3eafdc0cf164903cc9347161a3
Mateusz Brzeziński <mateusz0brzezinski@gmail.com>
2024-08-06 01:51:57 +0200

    chore(sticky-notes): update TodoInput component

--- src/components/modules/StickyNotes/TodoInput.tsx
+++ src/components/modules/StickyNotes/TodoInput.tsx
@@ -1,31 +1,38 @@
-import TrashIconSVG from "@/components/elements/svg/icons/interface/TrashIconSVG";
+import AddSquareSVG from "@/components/elements/svg/icons/interface/AddSquareSVG";
 import ButtonIcon from "@/components/ui/buttons/ButtonIcon";
+import { Input } from "@/components/ui/inputs/Input";
 import { cn } from "@/lib/utils";
+import { TStickyNoteColor } from "@/types/model-types";
 import React, { ChangeEvent } from "react";
 
 type TProps = {
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
   value: string;
   onClick: () => void;
+  color: TStickyNoteColor;
 };
 
-const TodoInput = ({ onChange, value, onClick }: TProps) => {
+const TodoInput = ({ onChange, value, onClick, color }: TProps) => {
   return (
-    <div className="input-container flex justify-center mb-4">
-      <input
-        type="text"
-        placeholder="new task"
+    <div className="input-container flex justify-center mt-4 gap-2 items-center">
+      <Input
+        className={cn(
+          `select-none sticky-note__textarea sticky-note__textarea--${color} active`,
+          "!rounded-none w-full text-base"
+        )}
         value={value}
         onChange={onChange}
-        className="p-2 text-lg border border-gray-300 rounded-lg w-full max-w-xs"
+        placeholder="New task"
+        type="text"
       />
+
       <ButtonIcon
         size="sm"
         className={cn(
-          "p-1 text-sm bg-primary text-primary-foreground rounded-lg"
+          "p-1 text-sm bg-background hover:bg-primary hover:text-primary-foreground active:translate-y-1 text-foreground rounded-lg w-12 h-10"
         )}
         onClick={onClick}
-        icon={<TrashIconSVG pathClass="stroke-white" />}
+        icon={<AddSquareSVG pathClass="stroke-white" />}
         tooltip={"Add"}
       />
     </div>
