264492b41df2ca3137a006e1d5ef704f9ef838e4
Mateusz Brzeziński <mateusz0brzezinski@gmail.com>
2024-10-27 00:00:45 +0200

    remove unused code

--- src/components/modules/Photos/Photos.tsx
+++ src/components/modules/Photos/Photos.tsx
@@ -1,26 +1,13 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useEffect } from "react";
 import {
-  ChevronLeft,
-  ChevronRight,
-  Star,
-  Heart,
-  Smile,
   Plus,
 } from "lucide-react";
-import { Input } from "@/components/ui/inputs/Input";
 import Button from "@/components/ui/buttons/Button";
 import Polaroid from "./Polaroid";
 import usePolaroidStore from "@/stores/zustand/usePolaroidStore";
-import usePolaroid from "@/hooks/usePolaroid";
 
 export default function Photos() {
-  const { polaroids, activeId, addNewPolaroid, updatePolaroid } =
-    usePolaroidStore();
-  const { fileInputRef, handleFileChange } = usePolaroid();
-
-  useEffect(() => {
-    console.log(polaroids);
-  }, [polaroids]);
+  const { polaroids, addNewPolaroid } = usePolaroidStore();
 
   return (
     <div className="z-50 absolute top-0 left-0 right-0 bottom-10 flex flex-col items-center justify-center min-h-screen p-4">
@@ -37,7 +24,7 @@ export default function Photos() {
           </Button>
         </div>
       </div>
-  
+
       <link
         href="https://fonts.googleapis.com/css2?family=Caveat&display=swap"
         rel="stylesheet"
