42199e5dbcff7dd364d859301f53940b936828cd
Mateusz Brzeziński <mateusz0brzezinski@gmail.com>
2024-11-01 22:55:10 +0100

    update cloudinary query hooks

--- src/stores/queries/useBgWallpapersQuery.tsx
+++ src/stores/queries/useBgWallpapersQuery.tsx
@@ -1,19 +1,29 @@
 import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
-const fetchWallpapers = async (tag: string) => {
-  const response = await fetch(
-    `https://res.cloudinary.com/${
-      import.meta.env.VITE_CLOUD_NAME
-    }/image/list/${tag}.json`
-  );
-  const data = await response.json();
-  return data.resources;
+// const fetchWallpapers = async (tag: string) => {
+//   const response = await fetch(
+//     `https://res.cloudinary.com/${
+//       import.meta.env.VITE_CLOUD_NAME
+//     }/image/list/${tag}.json`
+//   );
+//   const data = await response.json();
+//   return data.resources;
+// };
+
+const fetchWallpapers = async () => {
+  try {
+    const response = await axios.get(`${import.meta.env.VITE_API_URL}/cloudinary-images`);
+    return response.data.resources;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
 };
 
 const useBgWallpapersQuery = () => {
   return useQuery({
     queryKey: ["cloudinary", "wallpaper"],
-    queryFn: () => fetchWallpapers("wallpaper"),
+    queryFn: () => fetchWallpapers(),
   });
 };
 
