669a867160046228e4e4d8b1af54a16310b79eb2
Mateusz Brzeziński <mateusz0brzezinski@gmail.com>
2025-03-02 17:15:38 +0100

    chore(journal): update initialized title, add loader when adding new entry, handle active entry when added or deleted entry

--- src/components/modules/Journal/JournalLeftPanel.tsx
+++ src/components/modules/Journal/JournalLeftPanel.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"; // Import useEffect
+import { useEffect, useRef } from "react"; // Import useEffect
 import { NotebookPen, Plus } from "lucide-react";
 import JournalEntry from "./JournalEntry";
 import useAddJournalEntryMutation from "@/stores/supabase/journal/useAddJournalEntryMutation";
@@ -7,40 +7,71 @@ import { cn } from "@/lib/utils";
 import { ScrollArea } from "@/components/ui/ScrollArea/ScrollArea";
 import { useJournalStore } from "@/stores/zustand/useJournalStore";
 import { TJournalData } from "@/types/query-types";
+import { formatJournalDate } from "@/utils/functions/fn-date";
+import SpinnerSVG from "@/components/elements/svg/icons/interface/SpinnerSVG";
+import clsx from "clsx";
 
 const JournalLeftPanel = ({ data }: { data: TJournalData[] }) => {
   const { activeEntry, setActiveEntry } = useJournalStore();
   const { mutate: addJournalEntry, isPending: isPendingAdd } =
     useAddJournalEntryMutation();
 
+  const prevDataLengthRef = useRef(data?.length || 0);
+
   useEffect(() => {
-    if (data && data.length > 0 && activeEntry === -1) {
-      const lastEntry = data[data.length - 1];
-      setActiveEntry(lastEntry.id);
+    if (data && data.length > 0) {
+      const currentDataLength = data.length;
+
+      if (currentDataLength !== prevDataLengthRef.current) {
+        const lastEntry = data[data.length - 1];
+        setActiveEntry(lastEntry.id);
+      }
+
+      prevDataLengthRef.current = currentDataLength;
     }
-  }, [data, activeEntry, setActiveEntry]);
+  }, [data, setActiveEntry]);
+
+  const handleAddNew = () => {
+    addJournalEntry({
+      title: formatJournalDate(new Date()),
+      content: "",
+    });
+  };
 
   return (
-    <div className="p-8 pr-0 flex flex-col pointer-events-auto min-w-[280px] w-[280px]">
+    <div className="relative p-8 pr-0 flex flex-col pointer-events-auto min-w-[280px] w-[280px]">
+      {isPendingAdd && (
+        <div className="z-10 absolute left-0 right-0 bottom-0 top-0 opacity-30"></div> //overlay block
+      )}
       <h3 className="flex flex-row items-center text-xl gap-3 tracking-wide mb-8">
         <NotebookPen />
         Journal
       </h3>
       <button
         disabled={isPendingAdd}
-        onClick={() => {
-          addJournalEntry({
-            title: "New entry",
-            content: "",
-          });
-        }}
+        onClick={handleAddNew}
         className={cn(
-          "flex items-center gap-2 justify-center px-4 py-2 rounded-sm cursor-pointer transition duration-200 hover:border-primary hover:bg-primary hover:text-foreground-primary border-ransparent border active:translate-y-2",
+          "relative flex items-center gap-2 justify-center px-4 py-2 rounded-sm cursor-pointer transition duration-200 hover:border-primary hover:bg-primary hover:text-foreground-primary border-ransparent border active:translate-y-2",
           isPendingAdd && "opacity-50 cursor-default"
         )}
       >
-        <Plus />
-        <span className="text-lg truncate pr-4">Add new</span>
+        {isPendingAdd && (
+          <div className=" absolute left-1/2 -rotate-90 top-1/2 -translate-x-1/2 -translate-y-1/2">
+            <SpinnerSVG
+              className="origin-center"
+              pathClass="stroke-primary fill-primary"
+            />
+          </div>
+        )}
+        <div
+          className={clsx(
+            "flex flex-row gap-1 items-center",
+            isPendingAdd && "opacity-0"
+          )}
+        >
+          <Plus />
+          <span className="text-lg truncate pr-4">Add new</span>
+        </div>
       </button>
       <ScrollArea className="flex-grow min-h-0 overflow-y-auto mt-4">
         <div className="flex flex-col gap-2 pointer-events-auto">
--- src/utils/functions/fn-date.ts
+++ src/utils/functions/fn-date.ts
@@ -12,3 +12,24 @@ export const getWeekDays = () => {
 
   return weekDays;
 };
+
+export const formatJournalDate = (date: Date) => {
+  const options: Intl.DateTimeFormatOptions = {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  };
+  const formattedDate = date.toLocaleDateString("en-US", options);
+
+  const day = date.getDate();
+  const suffix =
+    day % 10 === 1 && day !== 11
+      ? "st"
+      : day % 10 === 2 && day !== 12
+      ? "nd"
+      : day % 10 === 3 && day !== 13
+      ? "rd"
+      : "th";
+
+  return formattedDate.replace(/\d+/, `${day}${suffix}`);
+};
